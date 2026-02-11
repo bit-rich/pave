08d32b598650ab629f9de2216637aab9a71cddf4
bit-rich <176977232+bit-rich@users.noreply.github.com>
2026-02-11 00:28:57 -0800

    testing firewall

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::UdpSocket;
+#[tokio::main]
+async fn main() {
+    let socket = UdpSocket::bind("[::]:9999").await.unwrap();
+    let mut buffer: [u8; 1024] = [0;1024];
+    let (byte_count,address) = socket.recv_from(&mut buffer).await.unwrap();
+    println!("Recieved {byte_count} bytes from {address}");
 }
