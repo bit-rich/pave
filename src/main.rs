bdaa3aeb2cf3eebd30afd244b86426c79a66884b
bit-rich <176977232+bit-rich@users.noreply.github.com>
2026-02-12 23:00:32 -0800

    add basic holepunching

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use tokio::net::UdpSocket;
+use std::fs::read_to_string;
+use tokio::time::{sleep, Duration};
+
 #[tokio::main]
 async fn main() {
+    let ip = read_to_string("peers.txt").unwrap().trim().to_string();
     let socket = UdpSocket::bind("[::]:9999").await.unwrap();
-    let mut buffer: [u8; 1024] = [0;1024];
-    let (byte_count,address) = socket.recv_from(&mut buffer).await.unwrap();
-    println!("Recieved {byte_count} bytes from {address}");
+    let (send_socket, recv_socket) = socket.split();
+
+    tokio::spawn(async move {
+        let mut buffer: [u8; 1024] = [0;1024];
+        loop{
+            let (byte_count,address) = recv_socket.recv_from(&mut buffer).await.unwrap();
+            println!("Recieved {byte_count} bytes from {address}");
+        }
+    });
+    loop{
+        sleep(Duration::from_secs(10)).await;
+        send_socket.send_to(b"hello",&ip).await.unwrap();
+    }
+
+
 }
