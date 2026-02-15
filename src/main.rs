f34b7d1a4ced511d43f3b6f4bf272b6c95881282
bit-rich <176977232+bit-rich@users.noreply.github.com>
2026-02-14 16:42:40 -0800

    replace .split() with Arc

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use tokio::net::UdpSocket;
 use std::fs::read_to_string;
 use tokio::time::{sleep, Duration};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     let ip = read_to_string("peers.txt").unwrap().trim().to_string();
     let socket = UdpSocket::bind("[::]:9999").await.unwrap();
-    let (send_socket, recv_socket) = socket.split();
-
+    let sender_socket = Arc::new(socket);
+    let receiver_socket = sender_socket.clone();
     tokio::spawn(async move {
         let mut buffer: [u8; 1024] = [0;1024];
         loop{
-            let (byte_count,address) = recv_socket.recv_from(&mut buffer).await.unwrap();
+            let (byte_count,address) = receiver_socket.recv_from(&mut buffer).await.unwrap();
             println!("Recieved {byte_count} bytes from {address}");
         }
     });
     loop{
         sleep(Duration::from_secs(10)).await;
-        send_socket.send_to(b"hello",&ip).await.unwrap();
+        sender_socket.send_to(b"hello",&ip).await.unwrap();
     }
 
 
